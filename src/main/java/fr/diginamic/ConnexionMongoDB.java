0c6410dc12d441e9318673c41a5e1c53ef39de7f
hdesain-diginamic <hugodesain@gmail.com>
2024-11-18 13:01:16 +0100

    Création des classes + Connexion DB + CRUD

--- src/main/java/fr/diginamic/ConnexionMongoDB.java
+++ src/main/java/fr/diginamic/ConnexionMongoDB.java
@@ -5,16 +5,26 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class ConnexionMongoDB {
+    private MongoClient mongoClient;
+    private MongoDatabase database;
 
-    public static MongoDatabase getDatabase(String databaseName) {
-        try {
-            String connectionString = "mongodb://localhost:27017";
-            MongoClient mongoClient = MongoClients.create(connectionString);
-            System.out.println("Connexion réussie à la base de données : " + databaseName);
-            return mongoClient.getDatabase(databaseName);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Erreur lors de la connexion à MongoDB");
-        }
+    // Constructeur qui établit la connexion
+    public ConnexionMongoDB() {
+        // Connexion au serveur MongoDB local
+        mongoClient = MongoClients.create("mongodb://localhost:27017");
+        // Accès à la base de données 'Royaume'
+        database = mongoClient.getDatabase("Royaume");
+        System.out.println("Connexion réussie à la base de données : " + database.getName());
+    }
+
+    // Méthode pour obtenir la base de données
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    // Méthode pour fermer la connexion
+    public void fermerConnexion() {
+        mongoClient.close();
+        System.out.println("Connexion fermée.");
     }
 }
--- src/main/java/fr/diginamic/Main.java
+++ src/main/java/fr/diginamic/Main.java
@@ -1,10 +1,34 @@
 package fr.diginamic;
+import entites.Citoyen;
+import entites.Ressource;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class Main {
     public static void main(String[] args) {
+
+        ConnexionMongoDB connexionMongoDB = new ConnexionMongoDB();
         try {
-            MongoDatabase database = ConnexionMongoDB.getDatabase("Royaume");
+            MongoDatabase database = connexionMongoDB.getDatabase();
+
+            MongoCollection<Document> Ressources = database.getCollection("ressources");
+
+            Ressources.insertOne(new Document("type", "Pierre").append("quantite", 200));
+            System.out.println("Ressource 'Pierre' ajoutée");
+
+            System.out.println("Lecture de toutes les ressources : " + Ressources.find());
+
+            Ressources.updateOne(
+                    new Document("type", "Bois"),
+                    new Document("$set", new Document("quantite", 350))
+            );
+            System.out.println("Mise à jour de la quantité de 'Bois'");
+
+            Ressources.deleteOne(new Document("type", "Pierre"));
+            System.out.println("Suppression de la ressource 'Pierre'");
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
